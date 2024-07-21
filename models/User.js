e03c07ae43e4f09a27b174bfdef642e93df4a3e2
MohamedEg36 <mohamed.egbaria.4@gmail.com>
2024-07-21 17:07:42 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessCode: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  preferences: {
+    startDate: Date,
+    endDate: Date,
+    destination: String,
+    holidayType: String,
+  },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
